5cc3a2b22ac5461073f2d94b960f1d1cd8f3e7a0
Andy Yan <andyshrk@gmail.com>
2021-11-07 18:15:42 +0800

    ovltest: Support YUV420_10BIT/8BIT AFBC test
    ovltest -s 185@119:800x1280 -P 122@119:320x240@YU10@afbc -F res/320x240_yuv420_10bit_afbc.bin
    
    Signed-off-by: Andy Yan <andyshrk@gmail.com>

--- tests/util/format.c
+++ tests/util/format.c
@@ -67,6 +67,9 @@ static const struct util_format_info format_info[] = {
 	{ DRM_FORMAT_YVU422, "YV16", MAKE_YUV_INFO(YUV_YCrCb, 2, 1, 1) },
 	{ DRM_FORMAT_YUV444, "YU24", MAKE_YUV_INFO(YUV_YCbCr, 1, 1, 1) },
 	{ DRM_FORMAT_YVU444, "YV24", MAKE_YUV_INFO(YUV_YCrCb, 1, 1, 1) },
+	/* YUV420_8BIT/YUV420_10BIT only used for AFBC test now */
+	{ DRM_FORMAT_YUV420_8BIT, "YU08", MAKE_YUV_INFO(YUV_YCrCb, 2, 2, 2) },
+	{ DRM_FORMAT_YUV420_10BIT, "YU10", MAKE_YUV_INFO(YUV_YCrCb, 2, 2, 2) },
 	/* RGB16 */
 	{ DRM_FORMAT_ARGB4444, "AR12", MAKE_RGB_INFO(4, 8, 4, 4, 4, 0, 4, 12) },
 	{ DRM_FORMAT_XRGB4444, "XR12", MAKE_RGB_INFO(4, 8, 4, 4, 4, 0, 0, 0) },
